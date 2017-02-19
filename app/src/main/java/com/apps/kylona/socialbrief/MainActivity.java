7ccb4414cb34e692e1f38e5dce68fcf165cc68c1
Kyle Storey <kyle.r.storey@gmail.com>
2017-02-19 02:39:13 -0700

    Add button link to new activity

--- app/src/main/java/com/apps/kylona/socialbrief/MainActivity.java
+++ app/src/main/java/com/apps/kylona/socialbrief/MainActivity.java
@@ -18,11 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-       /* Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+       // String message = editText.getText().toString();
+       // intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-        */
     }
 }
